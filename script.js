b1937e4733ae9ee0f560d856858044877ad4181f
BRAGADEESH_V <bragadeesh2005@gmail.com>
2023-11-20 00:03:10 +0530

    modify mobile ui

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-let menu= document.querySelector(".menu");
-let unorderList = document.querySelector('nav ul');
-menu.addEventListener('click',()=>{
-    unorderList.classList.toggle('showmenu')
-})
+// let menu= document.querySelector(".menu");
+// let unorderList = document.querySelector('nav ul');
+// menu.addEventListener('click',()=>{
+//     unorderList.classList.toggle('showmenu')
+// })
 
-const toTop=document.querySelector(".to-top");
-window.addEventListener("scroll",()=>{
-    if(window.pageYOffset > 100){
-        toTop.classList.add("active");
-    }else{
-        toTop.classList.remove("active");
-    }
-})
+// const toTop=document.querySelector(".to-top");
+// window.addEventListener("scroll",()=>{
+//     if(window.pageYOffset > 100){
+//         toTop.classList.add("active");
+//     }else{
+//         toTop.classList.remove("active");
+//     }
+// })
+let menu = document.querySelector(".menu");
+let unorderList = document.querySelector('nav ul');
+let menuLines = document.querySelectorAll('.menu-line');
+
+menu.addEventListener('click', () => {
+  unorderList.classList.toggle('showmenu');
+
+  // Toggle the active class on each menu line
+  menuLines.forEach(line => line.classList.toggle('active'));
+});
+
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
